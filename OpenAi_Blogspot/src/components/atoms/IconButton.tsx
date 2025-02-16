379879e718b14be86ca42bd5a152471d92b942c7
Srikar045 <140223544+Arknight007@users.noreply.github.com>
2025-02-16 22:33:21 +0530

    Delete OpenAi_Blogspot directory

--- OpenAi_Blogspot/src/components/atoms/IconButton.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonHTMLAttributes, ReactNode } from 'react'
-
-type Props = {
-  icon: ReactNode
-} & ButtonHTMLAttributes<HTMLButtonElement>
-
-export default function IconButton({ className, icon }: Props) {
-  return <button className={`btn ${className}`}>{icon}</button>
-}
--- OpenAi_Blogspot/src/core/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next'
-
-import { ReactElement, ReactNode } from 'react'
-import { AppProps } from '../models'
-
-export type AppPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement, appProps: AppProps) => ReactNode
-}
